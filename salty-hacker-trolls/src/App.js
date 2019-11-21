d587ea2364fe10c9d18e017b6b1e21cf19d4cace
cmruss <cmrussell08@gmail.com>
2019-11-21 16:33:55 -0500

    trying to deploy on zeit

--- salty-hacker-trolls/src/App.js
+++ salty-hacker-trolls/src/App.js
@@ -25,4 +25,13 @@ function App() {
   ) 
 }
 
+
+const queryString = require('query-string');
+
+const params = queryString.parse(document.location.search);
+const redirect = params.redirect; // this would be "abcdefg" if the query was "?redirect=abcdefg"
+if (document.location.pathname === '/' && redirect) {
+  document.location.assign(`${document.location.origin}/${redirect}`);
+}
+
 export default App;
